62e36d984152bc3453a778ad49d38400471ae86f
saber <sanz.armenta@gmail.com>
2016-12-02 22:32:33 -0700

    test commint

--- app/src/main/java/com/vendimia/sanz/lavendimia/Vendimia.java
+++ app/src/main/java/com/vendimia/sanz/lavendimia/Vendimia.java
@@ -23,14 +23,7 @@ public class Vendimia extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+       
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -65,7 +58,7 @@ public class Vendimia extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+         //comen
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
